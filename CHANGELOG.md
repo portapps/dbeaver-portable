5abd64a4dc99ef226697822f07d09d8905d6ba84
CrazyMax <crazy-max@users.noreply.github.com>
2018-11-16 19:25:07 +0100

    Upgrade to DBeaver 5.2.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 5.2.4-2 (2018/11/16)
+
+* Upgrade to DBeaver 5.2.4
+
 ## 5.2.1-1 (2018/09/29)
 
 * Initial version based on DBeaver 5.2.1
--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func init() {
 
 func main() {
 	Papp.AppPath = AppPathJoin("app")
-	Papp.DataPath = AppPathJoin("data")
+	Papp.DataPath = CreateFolder(AppPathJoin("data"))
 	Papp.Process = PathJoin(Papp.AppPath, "dbeaver.exe")
 	Papp.Args = []string{
 		"-data",
