25e6d3e1497be1a36e15b7bb4aee0337ec449219
CrazyMax <crazy-max@users.noreply.github.com>
2019-03-15 11:46:00 +0100

    Portapps 1.20.2

--- main.go
+++ main.go
@@ -8,57 +8,63 @@ import (
 
 	"github.com/magiconair/properties"
 	. "github.com/portapps/portapps"
+	"github.com/portapps/portapps/pkg/utl"
+)
+
+var (
+	app *App
 )
 
 func init() {
-	Papp.ID = "dbeaver-portable"
-	Papp.Name = "DBeaver"
-	Init()
+	var err error
+
+	// Init app
+	if app, err = New("dbeaver-portable", "DBeaver"); err != nil {
+		Log.Fatal().Err(err).Msg("Cannot initialize application. See log file for more info.")
+	}
 }
 
 func main() {
-	Papp.AppPath = AppPathJoin("app")
-	Papp.DataPath = CreateFolder(AppPathJoin("data"))
-	Papp.Process = PathJoin(Papp.AppPath, "dbeaver.exe")
-	Papp.Args = []string{
+	utl.CreateFolder(app.DataPath)
+	app.Process = utl.PathJoin(app.AppPath, "dbeaver.exe")
+	app.Args = []string{
 		"-data",
-		Papp.DataPath,
+		app.DataPath,
 		"-vm",
-		PathJoin(Papp.AppPath, "jre", "bin", "javaw.exe"),
+		utl.PathJoin(app.AppPath, "jre", "bin", "javaw.exe"),
 	}
-	Papp.WorkingDir = Papp.AppPath
 
-	driversPath := CreateFolder(PathJoin(Papp.DataPath, ".metadata", "drivers"))
-	logsPath := CreateFolder(PathJoin(Papp.DataPath, ".metadata", "logs"))
-	corePrefsPath := CreateFolder(PathJoin(Papp.DataPath, ".metadata", ".plugins", "org.eclipse.core.runtime", ".settings"))
-	corePrefsFile := PathJoin(corePrefsPath, "org.jkiss.dbeaver.core.prefs")
+	driversPath := utl.CreateFolder(app.DataPath, ".metadata", "drivers")
+	logsPath := utl.CreateFolder(app.DataPath, ".metadata", "logs")
+	corePrefsPath := utl.CreateFolder(app.DataPath, ".metadata", ".plugins", "org.eclipse.core.runtime", ".settings")
+	corePrefsFile := utl.PathJoin(corePrefsPath, "org.jkiss.dbeaver.core.prefs")
 
 	defaultProps := properties.NewProperties()
-	_, _, _ = defaultProps.Set("dialog.default.folder", formatPath(Papp.DataPath))
-	_, _, _ = defaultProps.Set("logs.debug.location", formatPath(PathJoin(logsPath, "dbeaver-debug.log")))
+	_, _, _ = defaultProps.Set("dialog.default.folder", formatPath(app.DataPath))
+	_, _, _ = defaultProps.Set("logs.debug.location", formatPath(utl.PathJoin(logsPath, "dbeaver-debug.log")))
 	_, _, _ = defaultProps.Set("qm.logDirectory", formatPath(logsPath))
 	_, _, _ = defaultProps.Set("ui.auto.update.check", "false")
 	_, _, _ = defaultProps.Set("ui.drivers.home", formatPath(driversPath))
 
-	if !Exists(corePrefsFile) {
-		Log.Info("Creating default props...")
-		if err := WriteToFile(corePrefsFile, defaultProps.String()); err != nil {
-			Log.Error("Cannot write default props to org.jkiss.dbeaver.core.prefs: ", err)
+	if !utl.Exists(corePrefsFile) {
+		Log.Info().Msg("Creating default props...")
+		if err := utl.WriteToFile(corePrefsFile, defaultProps.String()); err != nil {
+			Log.Error().Err(err).Msg("Cannot write default props to org.jkiss.dbeaver.core.prefs")
 		}
 	} else {
-		Log.Info("Loading org.jkiss.dbeaver.core.prefs file...")
+		Log.Info().Msg("Loading org.jkiss.dbeaver.core.prefs file...")
 		corePrefsProps, err := properties.LoadFile(corePrefsFile, properties.UTF8)
 		if err != nil {
-			Log.Error("Cannot load org.jkiss.dbeaver.core.prefs file: ", err)
+			Log.Error().Err(err).Msg("Cannot load org.jkiss.dbeaver.core.prefs file")
 		}
 		corePrefsProps.Merge(defaultProps)
-		Log.Info("Wrting to org.jkiss.dbeaver.core.prefs...")
-		if err := WriteToFile(corePrefsFile, corePrefsProps.String()); err != nil {
-			Log.Error("Cannot write to org.jkiss.dbeaver.core.prefs: ", err)
+		Log.Info().Msg("Writing to org.jkiss.dbeaver.core.prefs")
+		if err := utl.WriteToFile(corePrefsFile, corePrefsProps.String()); err != nil {
+			Log.Error().Err(err).Msg("Cannot write to org.jkiss.dbeaver.core.prefs")
 		}
 	}
 
-	Launch(os.Args[1:])
+	app.Launch(os.Args[1:])
 }
 
 func formatPath(path string) string {
